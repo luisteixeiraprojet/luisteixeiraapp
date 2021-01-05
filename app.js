d61e99409c6714f7435695c63306a22ce12e4377
luisteixeiraprojet <luisteixeiraprojet@gmail.com>
2021-01-05 23:09:01 +0100

    DB_connection

--- app.js
+++ app.js
@@ -19,6 +19,23 @@ app.get('/', (req, res) => {
 
 });
 
+
+//conexao DB
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+
+
 //Port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
