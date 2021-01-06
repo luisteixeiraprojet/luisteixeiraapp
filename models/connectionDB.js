500a0e6fc6d526bc0ca23d38d16b51487aa5ff11
luisteixeiraprojet <luisteixeiraprojet@gmail.com>
2021-01-06 23:45:04 +0100

    mysql2_changes_pool

--- models/connectionDB.js
+++ models/connectionDB.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'r6ze0q02l4me77k3.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
   user: 'sxfwfgg05lgz8lfg',
+  password:	'u5mxb6wi4kkxb8id',
   database: 'kfgv7183lcyrjg63',
   waitForConnections: true,
   connectionLimit: 10,
