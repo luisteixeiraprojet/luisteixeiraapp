db4b4025951bbebb29899cd9b669d4f83668b45b
luisteixeiraprojet <luisteixeiraprojet@gmail.com>
2021-01-13 22:12:25 +0100

    CORS solution

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
 
+//to 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 
 //Modules
 const employees = require('./routes/employees');
 const allTables = require('./models/tablesDB');
 
 
+
 //Middlewears - if a path is not defined by default it will be used in all of them 
 app.use(express.json()); //so we can hadle objects, ex. create a new user (always above the app.use modules)
 app.use('/employees', employees);  //path + router object: any routes started with /users use the router object imported inside the module users
+
+
 
 app.get('/', (req, res) => {
   console.log("get: http://localhost:3000/");
