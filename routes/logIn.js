b46c40f7cdc090559c4e4dfea56be98b615d470e
luisteixeiraprojet <luisteixeiraprojet@gmail.com>
2021-01-29 15:53:19 +0100

    logIn

--- routes/logIn.js
+++ routes/logIn.js
@@ -6,7 +6,4 @@ const router = express.Router(); //the router that will be used in app.js
 //Import other Modules
 const authenticationDao = require('../controlleur/authenticationDataFunctions.js');
 
-//LogIn
-router.post('/logIn') {
-    console.log("post no servidor: http://localhost:3000/logIn");
-}
+
