13e9f16bc6cd0a0cd74f396004e7e4f881d7bf90
luisteixeiraprojet <luisteixeiraprojet@gmail.com>
2021-01-29 17:03:40 +0100

    login

--- app.js
+++ app.js
@@ -9,29 +9,33 @@ app.use(cors());
 //Modules
 const employees = require('./routes/employees');
 const allTables = require('./models/tablesDB');
+const login = require('./routes/logIn');
 
 
 
 //Middlewears - if a path is not defined by default it will be used in all of them 
 app.use(express.json()); //so we can hadle objects, ex. create a new user (always above the app.use modules)
 app.use('/employees', employees);  //path + router object: any routes started with /employees use the router object imported inside the module employees
-app.use('/login', logIn);
+app.use('/login', login);
 
-app.get('/login', (req, res) => {
+
+//______________________________________________________
+
+//Login 
+app.get('/', (req, res) => {
   console.log("get: http://localhost:3000/login");
-  
- 
   res.send("Login OK ");
 });
 
 
-
+//Get All Employees
 app.get('/', (req, res) => {
   console.log("get: http://localhost:3000/");
   //get all employees
- 
   res.send("OK");
 });
+
+
 
 //Create All Tables
 app.get('/createTables', function (req, res) {
